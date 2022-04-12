d0026ad146f562964d2b3f3d35862737b71f62fd
Vishwas <baghel.vishwas0@gmail.com>
2022-04-12 23:16:43 +0530

    [vishwas]add: Factorial Of Given Number By Normal Method

---
+++ FactorialOfNumber/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialOfNumber
+{
+    public class Factorial
+    {
+        public void FindFactorial()
+        {
+            int i, result = 1 ;
+            Console.WriteLine("Enter any number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("Factorial of given number :" + result);
+        }
+    }
+}
---
+++ FactorialOfNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FactorialOfNumber
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial of a number");
+            Factorial obj = new Factorial();
+            obj.FindFactorial();
+        }
+    }
+    
+}
